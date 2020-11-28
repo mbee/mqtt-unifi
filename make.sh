c4252f34289c97986607071449b0ee90ff6ab6c8
Marc Bee <marc.bee@gmail.com>
2020-11-28 19:17:46 +0100

    adding arm64 arch

--- make.sh
+++ make.sh
@@ -17,3 +17,8 @@ echo build windows/amd64
 mkdir -p release/$VERSION/windows/amd64
 GOOS=windows GOARCH=amd64 go build $NAME.go
 mv $NAME.exe release/$VERSION/windows/amd64
+
+echo build linu/aarch64
+mkdir -p release/$VERSION/linux/aarch64
+GOOS=linux GOARCH=arm64 go build $NAME.go
+mv $NAME release/$VERSION/linux/aarch64
